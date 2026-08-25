46e57ac6d0a97255c8710ddb5ec53d00664e74fc
prince sharma <princeshrm002@gmail.com>
2026-08-25 18:51:38 +0530

    asynchandler added

---
+++ src/lib/utils/asynchandler.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server"
+import { ApiResponse } from "../responses/ApiResponse"
+import { ApiError } from "../errors/ApiError"
+
+type asyncFn=(
+    request:NextRequest
+  
+)=>Promise<NextResponse | void>
+
+export const asynchandler=(fn:asyncFn)=>async(request:NextRequest)=>{
+    try {
+        await fn(request);
+    } catch (error) {
+       if(error instanceof ApiError){
+        return NextResponse.json(
+            {
+                sucess:false,
+                message:error.message,
+                errors:error.errors
+            },
+            {status:error.statusCode}
+        )
+       }
+       console.error("Unknown error occurred",error);
+       return NextResponse.json(
+        {
+            sucess:false,
+            message:"Internal Server Error",
+            errors:error
+        },
+        {status:500}
+       )
+    }
+}
